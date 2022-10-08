8b31a39a42afb9a39b8a39761b479ce9e50dcc6e
no-compromise <4andrej@gmail.com>
2022-10-08 15:30:37 +0200

    ...

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,16 +1,25 @@
 
 import Navbar from "../../components/navbar/Navbar";
 import Sidebar from "../../components/sidebar/Sidebar";
+import Widget from "../../components/widget/Widget";
 import "./Home.scss";
 
 const Home = () => {
   return (
     <div className="home">
-       <Sidebar/>
-       <div className="homeContainer">
-        <Navbar/>
-        Some home stuff
-       </div>
+      <Sidebar />
+      <div className="homeContainer">
+        <Navbar />
+        <div className="widgets">
+          <Widget type="user" />
+          <Widget type="order" />
+          <Widget type="earnings" />
+          <Widget type="balance" />
+        </div>
+        <div className="charts">
+
+        </div>
+      </div>
     </div>
   )
 }
